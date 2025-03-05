dd6a2211b8d0fb50a789adf8e3a79c70add7073f
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-05 08:37:14 +0100

    Create Condicional If.js

---
+++ Condicional If.js
@@ -0,0 +1,15 @@
+let comerPizza = "bulbasur";
+
+if (comerPizza == false) {
+    console.log("no tengo pizza");
+} else if (comerPizza == true) {
+    colsole.log("tengo pizza");
+} else {
+    console.log(comerPizza)
+}
+
+let randomChar = "a";
+
+if (randomChar => "a" && randomChar < "m") {
+    console.log("va despues")
+}
